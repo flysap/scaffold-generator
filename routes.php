f7111349f516cdaddc4862e4c3a32ae773a74f30
Radu <radu.bordei89@gmail.com>
2015-07-29 14:25:26 +0300

    adding routes get / update file.

--- routes.php
+++ routes.php
@@ -7,11 +7,37 @@ Route::group(['prefix' => 'scaffold-generator'], function() {
     Route::match(['post', 'get'], 'generate', ['as' => 'scaffold-generate', function(Request $request) {
         $service = app('scaffold-generator');
 
-        if( $request->method() == 'POST' )
-            $service->generate(
+        $isGenerated = false;
+        $pathModule  = null;
+        if( $request->method() == 'POST' ) {
+            $isGenerated = true;
+
+            $pathModule = $service->generate(
                 $request->all()
             );
+        }
 
-        return view('scaffold-generator::generate');
+        return view('scaffold-generator::generate', compact('isGenerated', 'pathModule'));
+    }]);
+
+    /**
+     * Update the file .
+     *
+     */
+    Route::post('update-file', ['as' => 'update-file', function(Request $request) {
+        Flysap\FileManager\updateFile(
+            $request['file'],
+            $request['content']
+        );
+    }]);
+
+    /**
+     * Load the file .
+     *
+     */
+    Route::get('load-file', ['as' => 'load-file', function(Request $request) {
+        return Flysap\FileManager\getFile(
+            $request['file']
+        );
     }]);
 });
