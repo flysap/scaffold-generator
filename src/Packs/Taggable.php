e503dfd79254042d1b78f0c1489887b4c499202a
Radu <radu.bordei89@gmail.com>
2015-09-16 14:26:18 +0300

    adding taggable package.

---
+++ src/Packs/Taggable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Flysap\ScaffoldGenerator\Packs;
+
+class Taggable extends Package implements PackageInterface {
+
+    /**
+     * @return mixed
+     */
+    public function traits() {
+        return "    use TaggableTrait;\n";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function contracts() {
+        return ', TaggableInterface';
+    }
+
+    /**
+     * Get import data .
+     *
+     * @return mixed
+     */
+    public function import() {
+        return "use Cartalyst\\Tags\\TaggableTrait;\nuse Cartalyst\\Tags\\TaggableInterface;\n";
+    }
+}
